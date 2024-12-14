83bbb190b68d911f5d6a6c205550ad6d8c5b0260
M.A.H.K. Marasinghe <himanthamarasinghe@gmail.com>
2024-12-14 11:40:37 +0530

    Initial commit

---
+++ src/main/java/com/CipherChat/CipherChat/CipherChatApplication.java
@@ -0,0 +1,13 @@
+package com.CipherChat.CipherChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CipherChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CipherChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CipherChat/CipherChat/CipherChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CipherChat.CipherChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CipherChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
